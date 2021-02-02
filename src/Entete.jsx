6fea06c43fdbc6b01ac55fee82a52a5327b7b095
Abdirahman <e1703548@cmaisonnuenve.qc.ca>
2021-02-02 10:19:30 -0500

    Setup initial

---
+++ src/Entete.jsx
@@ -0,0 +1,18 @@
+export default function Entete(props){
+     const a = 5;
+     return(
+        <header>
+          <div>Logo</div>
+          <ul className="NavPrincipale">
+             <li>Produits</li>
+             <li>A propos de nous</li>
+          </ul>
+          <ul className="NavUtil">
+             <li>Produits</li>
+             <li>A propos de nous</li>
+             </ul>
+      </header>
+     );
+
+}
+
---
+++ src/Pied2Page.jsx
@@ -0,0 +1,8 @@
+export default function Entete(props){
+   
+    return(
+        <footer>
+      &copy; 2021 - TIM
+    </footer>
+    );
+}
