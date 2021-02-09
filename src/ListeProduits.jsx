f4cfd65022891ca273e537df22c4cffa61b4875d
Abdirahman <e1703548@cmaisonnuenve.qc.ca>
2021-02-09 14:21:33 -0500

    Cours 5

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,22 +1,26 @@
-import './ListeProduits.scss';
-import Produit from './Produit';
-import tabProduits from './data/produits.json'
+  
+import "./ListeProduits.scss";
+import Produit from "./Produit";
+import tabProduits from "./data/produits.json";
 
- function ListeProduits(props){
-     return(
-         <div className="ListeProduits">
-             <h2>Produits disponibles</h2>
-             <ul>
-                 {
-                    tabProduits.map((prd) =>
-                       <Produit id={prd.id} nom={prd.nom} prix={prd.prix} />
-                    )
-                 }
-                
-             </ul>
-         </div>
-     );
-
+function ListeProduits(props) {
+  //console.log("Panier dans ListeProduits", props.etatPanier);
+  return (
+    <div className="ListeProduits">
+      <h2>Produits disponibles</h2>
+      <ul>
+        {tabProduits.map((prd) => (
+          <Produit
+            key={prd.id}
+            etatPanier={props.etatPanier}
+            id={prd.id}
+            nom={prd.nom}
+            prix={prd.prix}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ListeProduits;
--- src/Produit.jsx
+++ src/Produit.jsx
@@ -1,20 +1,35 @@
 import BtnAjoutPanier from './BtnAjoutPanier';
 import './Produit.scss';
 
- function Produit(props){
-     return(
-         <li className="Produit">
-             <div className="image">
-                  <img src={'images-produits/'+ props.id +'.webp'} alt={props.nom}/> 
-             </div>
-                <div className="info">
-                     <h3>{props.nom}</h3>
-                     <p className="Prix">{props.prix} CA$</p>
-                      <BtnAjoutPanier/>
-                </div>
-         </li>
-     );
+export default function Produit(props) {
+    //console.log("Panier dans Produit : ", props.etatPanier);
 
+    /**
+     * Ajoute l'article au panier
+     */
+    function ajouterArticle() {
+        const [panier, setPanier] = props.etatPanier;
+        if(panier[props.id]) {
+            panier[props.id].qte++;
+        }
+        else {
+            panier[props.id] = {prix: props.prix, qte: 1}
+        }
+        // Maintenant il faut changer l'état du panier avec setPanier
+        setPanier(panier);
+        console.log(panier);
+    }
+
+    return (
+        <li className="Produit">
+            <div className="image">
+                <img src={'images-produits/' + props.id + '.webp'} alt={props.nom}/>
+            </div>
+            <div className="info">
+                <h3>{props.nom}</h3>
+                <p className="prix">{props.prix} $CA</p>
+                <BtnAjoutPanier onClick={ajouterArticle} />
+            </div>
+        </li>
+    );
 }
-
-export default Produit;
